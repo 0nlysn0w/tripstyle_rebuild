8858319ac8fff274c885ebf97df08d5884b297c1
Leon <joost.stam@gmail.com>
2019-01-10 15:26:44 +0100

    Add login implementation with redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,25 @@ import { NavLink } from 'react-router-dom';
 import SearchBar from './SearchBar'
 import LoginForm from './LoginForm'
 import ShoppingCart from './ShoppingCart'
-    
+import { connect } from 'react-redux';
+import { userActions } from '../redux/actions'
+
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+        let user = JSON.parse(localStorage.getItem("user"));
+        if (user) {
+            this.props.dispatch(userActions.remember(user));
+        }
+    }
+
+    logout = () => {
+        this.props.dispatch(userActions.logout())
+        // window.location.reload(true)
+    }
+
     render() {
         return (
             <Menu borderless size='massive' color='grey' inverted>
@@ -17,11 +34,21 @@ class Header extends Component {
                 <MenuItem position='right'>
                     <SearchBar></SearchBar>
                 </MenuItem>
-                <MenuItem className="Login" position='right'>
+                {this.props.user ? 
+                <MenuItem>
+                    <Button onClick={this.logout}>Logout</Button>
+                </MenuItem> 
+                : <MenuItem className="Login" position='right'>
 
-                    <LoginForm />
+                        <LoginForm />
 
-                </MenuItem>
+                    </MenuItem>}
+
+
+
+
+
+
                 <MenuItem>
                     <ShoppingCart />
                 </MenuItem>
@@ -30,4 +57,10 @@ class Header extends Component {
     }
 }
 
-export default Header;
+const mapStateToProps = state => {
+    return {
+        user: state.authentication.user
+    }
+}
+
+export default connect(mapStateToProps)(Header);
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Button, Form, Popup } from 'semantic-ui-react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { userActions } from '../redux/actions';
 
 class LoginForm extends Component {
 	constructor(props) {
@@ -8,26 +10,50 @@ class LoginForm extends Component {
 		this.state = {
 			email: '',
 			password: '',
-			submitted: false
+			submitted: false,
+			isOpen: false
 		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(e) {
+		const { name, value } = e.target;
+		this.setState({ [name]: value });
+	}
+
+    handleSubmit(e) {
+		console.log(this.state);
+        e.preventDefault();
+
+        this.setState({ submitted: true });
+        const { email, password } = this.state;
+        const { dispatch } = this.props;
+        if (email && password) {
+		   let v = dispatch(userActions.login(email, password));
+		   console.log(v)
+        }
+	}
+	
+	handleOpen() {
+		this.setState({isOpen: true})
 	}
 
 	render() {
-		const { loggingIn } = this.props;
+		const { loggingIn, isOpen } = this.props;
 		return (
-			<Popup wide trigger={
-				<Button icon='user' color='blue' circular />} on='click'>
+			<Popup wide trigger={<Button icon='user' color='blue' circular onClick={this.handleOpen.bind(this)} />} open={isOpen} on='click'>
 				{<Form onSubmit={this.handleSubmit}>
 					<Form.Field>
 						<label>Email</label>
-						<input id="email" type="text" placeholder='Email' required onChange={this.handleChange} />
+						<input name="email" type="text" placeholder='Email'  onChange={this.handleChange} />
 					</Form.Field>
 					<Form.Field>
 						<label>Password</label>
-						<input id="password" type="password" placeholder='Password' required onChange={this.handleChange} />
+						<input name="password" type="password" placeholder='Password'  onChange={this.handleChange} />
 					</Form.Field>
 
-					<Button positive fluid>Login</Button>
+					<Button positive fluid type="submit">Login</Button>
 					{loggingIn &&
 						<img alt="loading.." src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
 					}
@@ -38,4 +64,12 @@ class LoginForm extends Component {
 	}
 }
 
-export default LoginForm;
+function mapStateToProps(state) {
+	const { loggedIn, isOpen } = state.authentication;
+	return {
+		loggedIn,
+		isOpen
+	}
+}
+
+export default connect(mapStateToProps)(LoginForm);

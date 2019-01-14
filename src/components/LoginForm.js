83b91d950fd6e75d0dca21d30228f211bf0a9372
Joost Stam <joost.stam@gmail.com>
2019-01-14 11:58:48 +0100

    Add header in login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { userActions, alertActions } from '../redux/actions';
 import { history } from '../helpers/'
+import TopHeader from './Header'
 
 class LoginForm extends Component {
 	constructor(props) {
@@ -26,8 +27,8 @@ class LoginForm extends Component {
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-	LoginAdmin(){
-		if (this.state.email === 'admin' && this.state.password === 'admin'){
+	LoginAdmin() {
+		if (this.state.email === 'admin' && this.state.password === 'admin') {
 			window.location = "http://localhost:3000/admin/admin";
 			console.log('admin')
 		}
@@ -61,60 +62,64 @@ class LoginForm extends Component {
 		console.log('alert', alert.message)
 
 		return (
-			<Container>
-				<Divider hidden />
-				<Header size='huge'>Login</Header>
-				{alert.message &&
-					<div className={`alert ${alert.type}`}>{alert.message}</div>
-				}
+			<React.Fragment>
 
-				<Form onSubmit={this.handleSubmit}>
-					<Form.Field>
-						<label>Email</label>
-						<input name="email" type="text" placeholder='Email' onChange={this.handleChange} />
-						{submitted && !email &&
-                            <div className="help-block">Email is required</div>
-                        }
-					</Form.Field>
-					<Form.Field>
-						<label>Password</label>
-						<input name="password" type="password" placeholder='Password' onChange={this.handleChange} />
-						{submitted && !password &&
-                            <div className="help-block">Password is required</div>
-                        }
-					</Form.Field>
-
-					<Button positive fluid type="submit">Login</Button>
-					{loggingIn &&
-						<img alt="loading.." src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+				<TopHeader />
+				<Container>
+					<Divider hidden />
+					<Header size='huge'>Login</Header>
+					{alert.message &&
+						<div className={`alert ${alert.type}`}>{alert.message}</div>
 					}
 
-					<NavLink to='/register'>Don't have an account yet? register here.</NavLink>
-				</Form>
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-				<Divider hidden />
-			</Container>
+					<Form onSubmit={this.handleSubmit}>
+						<Form.Field>
+							<label>Email</label>
+							<input name="email" type="text" placeholder='Email' onChange={this.handleChange} />
+							{submitted && !email &&
+								<div className="help-block">Email is required</div>
+							}
+						</Form.Field>
+						<Form.Field>
+							<label>Password</label>
+							<input name="password" type="password" placeholder='Password' onChange={this.handleChange} />
+							{submitted && !password &&
+								<div className="help-block">Password is required</div>
+							}
+						</Form.Field>
+
+						<Button positive fluid type="submit">Login</Button>
+						{loggingIn &&
+							<img alt="loading.." src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+						}
+
+						<NavLink to='/register'>Don't have an account yet? register here.</NavLink>
+					</Form>
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+					<Divider hidden />
+				</Container>
+			</React.Fragment>
 		)
 	}
 }
